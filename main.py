6291e8b3a6fa9601d931bb11f35d69f66aa7d12c
Oleg <olegkatyskin@gmail.com>
2022-11-21 12:19:14 +0300

    123

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-# This is a sample Python script.
+import random
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
 
 
-# Press the green button in the gutter to run the script.
+class YellowCircle(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.do_paint = False
+        uic.loadUi('UI.ui', self)
+
+        self.pushButton.clicked.connect(self.action)
+
+    def action(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            d = random.randint(100, 200)
+            qp.setBrush(QColor(255, 255, 0))
+            qp.drawEllipse(100, 100, int(d / 2), int(d / 2))
+            qp.end()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print(0)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    prog = YellowCircle()
+    prog.show()
+    sys.exit(app.exec())
